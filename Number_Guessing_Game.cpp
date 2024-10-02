dc38d78ed3ea3179ca9a42cad5a3f40b64628944
AbhishekKulapkar <abhishekkulapkar@gmail.com>
2024-10-02 17:56:08 +0530

    completed first task

--- Number_Guessing_Game.cpp
+++ Number_Guessing_Game.cpp
@@ -9,11 +9,23 @@ using namespace std;
 
 
 int main(){
-     srand(time(NULL));
+     srand((unsigned int) time(NULL));
 
+     int number = (rand() % 100) + 1;
+     int guess = 0;
 
+    do{
+        cout << "Guess a number between 1 and 100: ";
+        cin >> guess;
 
-
-
+        if(guess > number){
+            cout << "Too high!" << endl;
+        }else if(guess < number){
+            cout << "Too low!" << endl;
+        }else{
+            cout << "You guessed it! congratulations you won!" << endl;
+        }
+    }while(guess != number);
+    
     return 0;
 }
